7f51aba68406f544b0a6ecad058e6c44902543c1
inerc <inerc@users.noreply.github.com>
2025-11-18 19:44:18 +0300

    Deploy with Formspree

---
+++ precache-manifest.c62e9160c5624d931bd5b5d29028a4bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dae6fda2bf6e3003603afb3f600f410",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01adbe3b6c19e98a5b58",
+    "url": "/static/css/main.8acd1c10.chunk.css"
+  },
+  {
+    "revision": "0260e6efb406498a9f97",
+    "url": "/static/js/2.0719f04a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0719f04a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01adbe3b6c19e98a5b58",
+    "url": "/static/js/main.e1e9079c.chunk.js"
+  },
+  {
+    "revision": "f828d8acfcf24b73ee83",
+    "url": "/static/js/runtime-main.bf2d5c11.js"
+  }
+]);
