30e6b95d975b0ba49d3b2406293c2cc5963716c7
inerc <inerc@users.noreply.github.com>
2026-02-02 17:43:15 +0300

    Updates

---
+++ precache-manifest.35c1586119fdb29832d0a5d82911a1a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bd29ee70fd28c39ddf32d366d07407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bbd969ecf84eb5df537",
+    "url": "/static/css/main.9b59c9c5.chunk.css"
+  },
+  {
+    "revision": "0260e6efb406498a9f97",
+    "url": "/static/js/2.0719f04a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0719f04a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbd969ecf84eb5df537",
+    "url": "/static/js/main.febb0869.chunk.js"
+  },
+  {
+    "revision": "f828d8acfcf24b73ee83",
+    "url": "/static/js/runtime-main.bf2d5c11.js"
+  }
+]);
